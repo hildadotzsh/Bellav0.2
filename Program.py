bd1ddbbc61399dac05f16ecb05be291cd1422b35
Flames Elite <84363801+FlamesLLC@users.noreply.github.com>
2022-01-10 18:04:45 -0800

    * Request to upload v2.0x > 6:10 PM PST 1.10.22

---
+++ Program.py
@@ -0,0 +1,163 @@
+### link the bella.chr file to the program
+import os as os
+import sys as sys
+## write a way to contact the .chr file to connect to the bot and the program
+## look for the bella.chr file in the same directory as the program
+## if the file is not found, then ask the user to locate the file
+## if the file is found, then connect to the bot
+os.file_path = os.path.dirname(os.path.realpath(__file__))
+os.file_path = os.path.join(os.file_path, "bella.chr")
+if os.path.isfile(os.file_path):
+    print("file found")
+    print("connecting to the bot")
+    ## connect to the bot
+    ## test the connection
+    ## if the connection is successful, then start the program
+    ## if the connection is not successful, then ask the user to try again
+    switch = input("Would you like to boot up bella.ai? ")
+    if switch == "Yes":
+        print("connecting to the bot")
+        ## connect to the bot
+        ## test the connection
+        ## if the connection is successful, then start the program
+        ## if the connection is not successful, then ask the user to try again
+        print("Would  you like to boot up bella.ai?")
+        print("1. Yes")
+        print("2. No")
+        ## ask the user to boot up the bot
+        ## if yes look for chr file
+        ## if no, then exit the program
+        ## if the file is not found, then ask the user to locate the file
+        ## if the file is found, then connect to the bot
+        ## if the connection is successful, then start the program
+        os.file_path = os.path.dirname(os.path.realpath(__file__))
+        os.file_path = os.path.join(os.file_path, "bella.chr")
+        ## if bella.chr is found say "Hello, $computername"
+        # else exit program
+        ## make a switch stateemnt thhat chekcs yes or no if bella.chr is found then say "Howdy $computername")
+        ## make a way to check if the file is found the ai is online with bella.chr and
+        ## if the file is not found, then ask the user to locate the file
+        ## if the file is found, then connect to the bot 
+        ## connect to the bot
+        ## test the connection
+        ## if the connection is successful, then start the program
+        ## if the connection is not successful, then ask the user to try again
+        print("Would  you like to boot up bella.ai?")
+        print("1. Yes")
+        print("2. No")
+        ## ask the user to boot up the bot
+        ## if yes look for chr file
+        ## if no, then exit the program
+        ## if the file is not found, then ask the user to locate the file
+        ## if the file is found, then connect to the bot
+        ## if the connection is successful, then start the program
+        os.file_path = os.path.dirname(os.path.realpath(__file__))
+        os.file_path = os.path.join(os.file_path, "bella.chr")
+        ## if bella.chr is found say "Hello, $computername"
+        # else exit program
+        ## make a switch stateemnt thhat chekcs yes or no if bella.chr is found then say "Howdy $computername")
+        ## make a way to check if the file is found the ai is online with bella.chr and
+        ## if the file is not found, then ask the user to locate the file
+        ## if the file is found, then connect to the bot
+        ## if the connection is successful, then start the program
+ ## write a hello world message to see if the bot is alive
+print("Welcome to bella.ai")
+## do not display the message if the .chr file is gone
+## if the .chr file is gone, then exit the program
+ ## write a ai module that generates a tts message from input
+print("What would you like to say to bella.ai?")
+## speak the messag
+## write a question if you would like to netcat into a local computer that is windows or mac
+print("Hey this feature is in beta. would you like to netcat and scan the local computers?")
+netcat=input
+port=input
+## uses the netcat and port to scan local cameras and the shodan api with your link to the bot and display it in a windows
+## if you would like to netcat into a local computer that is windows or mac
+ ## write a question if you would like to netcat into a local computer that is windows or mac
+print("Hey this feature is in beta. would you like to netcat and scan the local computers?")
+netcat=input
+port=input
+## uses the netcat and port to scan local cameras and the shodan api with your link to the bot and display it in a windows
+## if you would like to netcat into a local computer that is windows or mac
+import shodan as shodan
+import requests as requests
+print("Would you like to interact with shodan?")
+switch = input("Would you like to interact with shodan? ")
+if switch == "Yes":
+    print("Would you like to search for a specific keyword?")
+    switch = input("Would you like to search for a specific keyword? ")
+    if switch == "Yes":
+        print("What keyword would you like to search for?")
+        keyword = input("What keyword would you like to search for? ")
+        print("Would you like to search for a specific port?")
+        switch = input("Would you like to search for a specific port? ")
+        if switch == "Yes":
+            print("What port would you like to search for?")
+            port = input("What port would you like to search for? ")
+            print("Would you like to search for a specific IP?")
+            switch = input("Would you like to search for a specific IP? ")
+            if switch == "Yes":
+                print("What IP would you like to search for?")
+                ip = input("What IP would you like to search for? ")
+                print("Would you like to search for a specific country?")
+                switch = input("Would you like to search for a specific country? ")
+                if switch == "Yes":
+                    print("What country would you like to search for?")
+                    country = input("What country would you like to search for? ")
+                    print("Would you like to search for a specific organization?")
+                    switch = input("Would you like to search for a specific organization? ")
+                    if switch == "Yes":
+                        print("What organization would you like to search for?")
+                        organization = input("What organization would you like to search for? ")
+                        print("Would you like to search for a specific asn?")
+                        switch = input("Would you like to search for a specific asn? ")
+                        if switch == "Yes":
+                            print("What asn would you like to search for?")
+                            asn = input("What asn would you like to search for? ")
+                            print("Would you like to search for a specific city?")
+                            switch = input("Would you like to search for a specific city? ")
+                            if switch == "Yes":
+                                print("What city would you like to search for?")
+                                ## ask the user all the questionsu
+                                city = input("What city would you like to search for? ")
+                                print("Would you like to search for a specific region?")
+                                switch = input("Would you like to search for a specific region? ")
+                                if switch == "Yes":
+                                    print("What region would you like to search for?")
+                                    region = input("What region would you like to search for? ")
+                                    print("Would you like to search for a specific postal code?")
+                                    switch = input("Would you like to search for a specific postal code? ")
+                                    if switch == "Yes":
+                                        print("What postal code would you like to search for?")
+                                        postal_code = input("What postal code would you like to search for? ")
+                                        print("Would you like to search for a specific latitude?")
+                                        switch = input("Would you like to search for a specific latitude? ")
+                                        if switch == "Yes":
+                                            print("What latitude would you like to search for?")
+                                            latitude = input("What latitude would you like to search for? ")
+                                            print("Would you like to search for a specific longitude?")
+                                            switch = input("Would you like to search for a specific longitude? ")
+                                            if switch == "Yes":
+                                                print("What longitude would you like to search for?")
+                                                longitude = input("What longitude would you like to search for? ")
+                                                print("Would you like to search for a specific timezone?")
+                                                switch = input("Would you like to search for a specific timezone? ")
+                                                if switch == "Yes":
+                                                    print("What timezone would you like to search for?")
+                                                    timezone = input("What timezone would you like to search for? ")
+                                                    print("Would you like to search for a specific isp?")
+                                                    switch = input("Would you like to search for a specific isp? ")
+                                                    if switch == "Yes":
+                                                        print("What isp would you like to search for?")
+                                                        isp = input("What isp would you like to search for? ")
+                                                        print("Would you like to search for a specific org?")
+                                                        switch = input("Would you like to search for a specific org? ")
+                                                        ## write a shodan api client for a jarvis bot
+                                                        ## use the commands to tunnel into the desired location
+                                                        if switch == "No": print("Okay I shall'nt search for an org")
+
+                                                        ### make a gradio gui app for interacting with a randomly picture of a female student anime girl on thisisnotexistis
+                                                        import gradio as grad
+                                                        gradio = grad.Gradio()  
+
+                                        print("Welcome to Bella's CPanel v0.2 by @Bella_AI")
